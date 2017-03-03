b80aabf5faea17b24d9c9207f01d38964ab40e52
josh-nissen <josh.e.nissen@outlook.com>
2017-03-03 12:16:42 -0500

    Add files via upload

---
+++ createPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Baseball
+{
+    public partial class createPlayer : Form
+    {
+        public createPlayer()
+        {
+            var query = "SELECT TeamName FROM Teams";
+            InitializeComponent();
+            FillData(query);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
